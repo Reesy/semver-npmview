8aecc36866d2562730a4e5da115a65fd4f5ab845
James Rees <james.rees@outlook.com>
2017-09-17 21:29:41 +0100

    Added a function to reconstruct prerelease versions to get around versions being unsorted

--- index.js
+++ index.js
@@ -23,16 +23,29 @@ exports.getLatest = function(desiredPackage, versions, prerelease){
 				}
 			}
 			if(alphaVersions.length > 1){
-				return alphaVersions[alphaVersions.length-1]
+				//return alphaVersions[alphaVersions.length-1]
+				return this.reconstructVersion(versions, prerelease, alphaVersions.length - 1)
 			}
 			return alphaVersions[0];
 		}).catch((error) => {
-
+			console.log("inside error catch" + error);
 			return "";
 		});
 }
+/**
+ * @name reconstructVersion
+ * @desc This function will reconstruct the string output as sort order of packages is non deterministic
+ * as such this will take the number of 'tags' found for a version such as 'alpha' or 'beta' and use the length as
+ * the number of packages currently published
+ * @param version - The version of wish to find latest
+ * @param prereleaseTag - The potential prerelease version specified
+ * @param latestPublishNumber - Iteration of the the specified package 
+ */
+exports.reconstructVersion = function(version, prereleaseTag, latestPublishNumber){
+	return version + '-' + prereleaseTag + "." + latestPublishNumber;
+}
 
-// this.getLatest("crapexample", "1.0.0", "alpha")
+// this.getLatest("sigma-cpq-quote", "2.5.0", "Pricing")
 // 	.then((result) => {
 // 		console.log(result);
 // 		return result;
